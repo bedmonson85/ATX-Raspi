392b04d96bba4b3e6a72d58a6bdf7ae6b7dae8ce
bedmonson85 <bedmonson85@gmail.com>
2015-02-21 16:11:41 -0500

    Update shutdownchecksetup.sh

--- shutdownchecksetup.sh
+++ shutdownchecksetup.sh
@@ -1,23 +1,23 @@
 echo '#!/bin/bash
 
-#This is GPIO 7 (pin 26 on the pinout diagram).
+#This is GPIO 28 (P5-3 on the pinout diagram).
 #This is an input from ATXRaspi to the Pi.
 #When button is held for ~3 seconds, this pin will become HIGH signalling to this script to poweroff the Pi.
-SHUTDOWN=7
+SHUTDOWN=28
 echo "$SHUTDOWN" > /sys/class/gpio/export
 echo "in" > /sys/class/gpio/gpio$SHUTDOWN/direction
 
-#This is GPIO 8 (pin 24 on the pinout diagram).
+#This is GPIO 29 (P5-4 on the pinout diagram).
 #This is an output from Pi to ATXRaspi and signals that the Pi has booted.
 #This pin is asserted HIGH as soon as this script runs (by writing "1" to /sys/class/gpio/gpio8/value)
-BOOT=8
+BOOT=29
 echo "$BOOT" > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio$BOOT/direction
 echo "1" > /sys/class/gpio/gpio$BOOT/value
 
 echo "ATXRaspi shutdown script started: asserted pins ($SHUTDOWN=input,LOW; $BOOT=output,HIGH). Waiting for GPIO$SHUTDOWN to become HIGH..."
 
-#This loop continuously checks if the shutdown button was pressed on ATXRaspi (GPIO7 to become HIGH), and issues a shutdown when that happens.
+#This loop continuously checks if the shutdown button was pressed on ATXRaspi (GPI28 to become HIGH), and issues a shutdown when that happens.
 #It sleeps as long as that has not happened.
 while [ 1 ]; do
   shutdownSignal=$(cat /sys/class/gpio/gpio$SHUTDOWN/value)
